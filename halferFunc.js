9577cfe27dd4c059755deb7c9b027bb3e1429efd
amethystcoder <amethystguitaristj@gmail.com>
2025-10-22 14:09:53 +0100

    did some more styling on the main page

--- halferFunc.js
+++ halferFunc.js
@@ -1,8 +1,23 @@
 
+
+const turnableData =[{
+    heading: "The Sleekest Cars",
+    body: ""
+}]
 /** 
- * @param {HTMLElement} elem
+ * @param {{id: number, reference: HTMLElement}} elemData
 */
-const resizeHalfer = (elem)=>{
-    elem.classList.add("widened-turnable");
-    elem.classList.remove("turnable-image")
+const resizeHalfer = (elemData)=>{
+    const widenedTurnable = document.getElementById("widened-turnable");
+    widenedTurnable.style.display = "flex";
+    widenedTurnable.style.background = `linear-gradient(to bottom, #00000063, #000000),${elemData.reference.style.backgroundImage }`;
+    widenedTurnable.style.backgroundSize = "cover";
+    widenedTurnable.style.backgroundPosition = "center"
+    document.body.style.overflow = "hidden"
+}
+
+const removeTurnable = ()=>{
+    const widenedTurnable = document.getElementById("widened-turnable");
+    widenedTurnable.style.display = "none";
+    document.body.style.overflow = "auto"
 }
