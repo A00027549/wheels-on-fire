1d93a62f29ad030f56e2366f4a82d7bc19f00061
amethystcoder <amethystguitaristj@gmail.com>
2025-11-26 14:35:59 +0000

    made some other new changes

--- events/collectionScript.js
+++ events/collectionScript.js
@@ -1,1 +1,22 @@
+let days = 10;
+let hours = 20;
+let minutes = 60;
+let seconds = 60;
 
+window.addEventListener("DOMContentLoaded",(ev)=>{
+    const dayCont = document.getElementById("day-text")
+    const hourCont = document.getElementById("hour-text")
+    const minuteCont = document.getElementById("minute-text")
+    const secondCont = document.getElementById("second-text")
+
+    setInterval(()=>{
+        seconds--;
+        if (seconds == 0) {seconds = 59;minutes--;}
+        if (minutes == 0) {minutes = 59;hours--;}
+        if (hours == 0) {hours = 23;days--;}
+        dayCont.innerText = days.toString()
+        hourCont.innerText = hours.toString()
+        minuteCont.innerText = minutes.toString()
+        secondCont.innerText =seconds.toString()
+    }, 1000);
+})
